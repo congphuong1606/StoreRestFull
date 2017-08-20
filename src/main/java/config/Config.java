92728b1d5852772ed8954d978df57603b0b1f73e
congphuong1606 <cphuong.coldboy@gmail.com>
2017-08-20 20:04:08 +0700

    demo

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,18 @@
+//package config;
+//
+//
+//
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class Config {
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//		return (container->{
+//			container.setPort(8080);
+//		});
+//	}
+//
+//}
